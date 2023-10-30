6644c57c638a63cd66e02543f087501172c63c49
Cezar <cezarguimaraes@gmail.com>
2023-10-30 15:43:04 -0300

    cronometers logic was applyed and is working ok

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,17 +1,54 @@
+import { useState, useEffect } from 'react';
+
+
 function Pomodoro() {
-  function setTimer() {
-    
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(59);
+  const [hours, setHours] = useState(0);
+  const [timerActive, setTimerActive] = useState(false);
+
+  //This is a switch to set when the timer is running or not
+  function playStopToogle() {
+    setTimerActive( (prevTimerActive) => !prevTimerActive);
   }
+
+  useEffect(() => {
+    let intervalId;
+
+    if (timerActive) {
+      if (minutes > 59) {
+        setMinutes(0);
+        setHours((prevHours) => prevHours + 1);
+      }
+
+      if (seconds > 59) {
+        setSeconds(0);
+        setMinutes((prevMinutes) => prevMinutes + 1)
+      }
+
+      intervalId = setInterval(() => {
+        setSeconds((prevSegundos) => prevSegundos + 1);
+      }, 1000);
+    
+      
+    
+    return () => {
+      clearInterval(intervalId);
+    };
+  }
+  }, [timerActive, seconds, minutes]);
+
+
 
   return (
     <div className="container">
       <div class="pomodoro-timer">
         <h2>Cronômetro Pomodoro</h2>
         <div class="timer">
-            <span id="minutes">25</span>:<span id="seconds">00</span>
+            <span>{ hours }</span><span>:</span><span>{ minutes }</span><span>:</span><span>{ seconds }</span>
         </div>
         <div class="controls">
-            <button id="start/pause">Iniciar</button>
+            <button id="start/pause" onClick={playStopToogle}>Iniciar</button>
             <button id="pause">Pausar</button>
         </div>
     </div>
